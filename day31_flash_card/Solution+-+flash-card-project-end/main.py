8a1e24118932fa7f4a6c93e5827248eb02937ee7
Wunna <wannamoesan11@gmail.com>
2026-01-30 12:20:01 +0700

    SMTP--Simple Mail Transfer Protocol

--- day31_flash_card/Solution+-+flash-card-project-end/main.py
+++ day31_flash_card/Solution+-+flash-card-project-end/main.py
@@ -7,14 +7,14 @@ current_card = {}
 to_learn = {}
 
 try:
-    data = pandas.read_csv("data/words_to_learn.csv")
+    data = pandas.read_csv("Solution+-+flash-card-project-end/data/words_to_learn.csv")
 except FileNotFoundError:
-    original_data = pandas.read_csv("data/french_words.csv")
+    original_data = pandas.read_csv("Solution+-+flash-card-project-end/data/french_words.csv")
     print(original_data)
     to_learn = original_data.to_dict(orient="records")
 else:
     to_learn = data.to_dict(orient="records")
-
+ 
 
 def next_card():
     global current_card, flip_timer
@@ -36,7 +36,7 @@ def is_known():
     to_learn.remove(current_card)
     print(len(to_learn))
     data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv", index=False)
+    data.to_csv("Solution+-+flash-card-project-end/data/words_to_learn.csv", index=False)
     next_card()
 
 
@@ -47,19 +47,19 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="images/card_front.png")
-card_back_img = PhotoImage(file="images/card_back.png")
+card_front_img = PhotoImage(file="Solution+-+flash-card-project-end/images/card_front.png")
+card_back_img = PhotoImage(file="Solution+-+flash-card-project-end/images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-cross_image = PhotoImage(file="images/wrong.png")
+cross_image = PhotoImage(file="Solution+-+flash-card-project-end/images/wrong.png")
 unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
-check_image = PhotoImage(file="images/right.png")
+check_image = PhotoImage(file="Solution+-+flash-card-project-end/images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
